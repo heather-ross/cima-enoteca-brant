89dce2bd387cd5c02fe23afbfd6a759d2007c41b
Heather Ross <hross299@yahoo.com>
2022-03-27 09:58:01 -0600

    Update HOO

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,24 @@ const Footer = () => {
         <footer className="footer">
             <div className="footer__cols">
                 <section className="footer__col">
-                    <h4 className="footer__col-title">SOCIAL</h4>
+                <h4 className="footer__col-title">HOURS</h4>
+                    <div className="footer__col-hours">
+                        <h5 className="footer__col-heading">TUESDAY - THURSDAY</h5>
+                        <p className="footer__col-text">11:30AM - 3PM | 5PM - 9:30PM</p>
+                        <h5 className="footer__col-heading">FRIDAY - SATURDAY</h5>
+                        <p className="footer__col-text">11:30AM - 3PM | 5PM - 10:30PM</p>
+                        <h5 className="footer__col-heading">MONDAY - SUNDAY</h5>
+                        <p className="footer__col-text">Closed</p>
+                    </div>
+                    
+                </section>
+                <section className="footer__col">
+                    <h4 className="footer__col-title">CONTACT</h4>
+                    <div className="footer__col-contact">
+                        <p className="footer__col-text">1200 Brant St., Burlington, ON</p>
+                        <a className="footer__col-text" href="tel:905-332-2022"><p className="footer__col-text" >(905) 332-2022</p></a>
+                    </div>
+                    <h4 className="footer__col-title footer__top-spacing">SOCIAL</h4>
                     <div className="footer__col-icons">
                         <a href="https://www.instagram.com/cimaonbrant/?hl=en" rel="noreferrer" target="_blank">
                             <img className="footer__col-icon" src={instagram} alt="Instagram icon" />
@@ -20,23 +37,17 @@ const Footer = () => {
                         </a>
                     </div>
                 </section>
-                <section className="footer__col">
-                    <h4 className="footer__col-title">CONTACT</h4>
-                    <div className="footer__col-contact">
-                        <p className="footer__col-text">1200 Brant St., Burlington, ON</p>
-                        <a className="footer__col-text" href="tel:905-332-2022"><p className="footer__col-text" >(905) 332-2022</p></a>
-                    </div>
-                </section>
-                <section className="footer__col">
+                {/* <section className="footer__col">
                     <h4 className="footer__col-title">HOURS</h4>
                     <div className="footer__col-hours">
-                        <h5 className="footer__col-heading">WEDNESDAY - SATURDAY</h5>
-                        <p className="footer__col-text">5PM - 10PM</p>
-                        <h5 className="footer__col-heading">SUNDAY - TUESDAY</h5>
+                        <h5 className="footer__col-heading">TUESDAY - THURSDAY</h5>
+                        <p className="footer__col-text">11:30AM - 3PM | 5PM - 9:30PM</p>
+                        <h5 className="footer__col-heading">FRIDAY - SATURDAY</h5>
+                        <p className="footer__col-text">11:30AM - 3PM | 5PM - 10:30PM</p>
+                        <h5 className="footer__col-heading">MONDAY - SUNDAY</h5>
                         <p className="footer__col-text">Closed</p>
                     </div>
-
-                </section>
+                </section> */}
             </div>
             <div className="footer__bottom-wrap">
             <p className="footer__copy">© <span>{new Date().getFullYear()}</span> CIMA ENOTECA / Design <a href="https://hross.dev" rel="noreferrer" target="_blank">hrDEV</a></p>
